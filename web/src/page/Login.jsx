f4d1dc0a0c47cf9e9d2c012c2bfb6856ea6eb4ed
sillyY <965768042@qq.com>
2018-04-09 17:36:16 +0800

    complete css

--- web/src/page/Login.jsx
+++ web/src/page/Login.jsx
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {withRouter} from "react-router-dom";
 
 import * as axios from 'axios';
-
 import '../assets/css/login.css';
 class Login extends Component {
 
@@ -10,7 +9,9 @@ class Login extends Component {
     super(props);
     this.state = {
       username: '',
-      password: ''
+      username_prompt: "",
+      password: '',
+      password_prompt: ""
     }
     this.handleChange = this
       .handleChange
@@ -19,12 +20,41 @@ class Login extends Component {
       .handleClick
       .bind(this);
   }
-  handleChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
 
+  handleChange(event) {
+    const target = event.target,
+      value = target.value,
+      name = target.name;
+    //username 输入提示 字母或数字，4到40位。
+    if (name === "username") {
+      if (this.checkUsername(value)) {
+        this.setState({username_prompt: ""})
+      } else {
+        this.setState({username_prompt: "字母或数字，4到40位!"})
+      }
+    }
+    //password 输入提示 6到40位。
+    if (name === "password") {
+      if (this.checkPassword(value)) {
+        this.setState({password_prompt: ""})
+      } else {
+        this.setState({password_prompt: "6到40位!"})
+      }
+    }
     this.setState({[name]: value});
+  }
+  checkUsername(value) {
+    var match = /^[A-Za-z0-9]{4,40}$/g;
+    if (match.test(value) ) {
+      return true;
+    }
+    return false;
+  }
+  checkPassword(value) {
+    if ((value.length >= 6 && value.length <= 40) ) {
+      return true;
+    }
+    return false;
   }
   async handleClick() {
     //测试使用:情况localstorage window.localStorage.clear();
@@ -34,65 +64,67 @@ class Login extends Component {
 
     //请求登陆接口
     try {
-      let response = await axios.post('http://localhost:8080/login', {username, password})
-      //localStorage存储token
-      switch (response.data.status) {
-        case 2000:
-          //存储token
-          window
-            .localStorage
-            .setItem('token_s', response.data.token.token_s);
-          window
-            .localStorage
-            .setItem('token_l', response.data.token.token_l);
-
-          //跳转路由，并将用户数据传到首页.
-          this
-            .props
-            .history
-            .push('/');
-          break;
-        case 2001:
-          alert('用户账号不存在或密码错误');
-          break;
-        default:
-          break;
+      if(this.checkUsername(this.state.username)&&this.checkPassword(this.state.password)){
+        let response = await axios.post('http://localhost:8080/login', {username, password})
+        //localStorage存储token
+        switch (response.data.status) {
+          case 2000:
+            //存储token
+            window
+              .localStorage
+              .setItem('token_s', response.data.token.token_s);
+            window
+              .localStorage
+              .setItem('token_l', response.data.token.token_l);
+  
+            //跳转路由，并将用户数据传到首页.
+            this
+              .props
+              .history
+              .push('/');
+            break;
+          case 2001:
+            alert('用户账号不存在或密码错误');
+            break;
+          default:
+            break;
+        }
       }
-
     } catch (error) {
       console.log(error);
     }
   }
-
   render() {
     return (
       <div className="login">
-      <div className="container">
-      <div className="block title">
-        用户登陆
-      </div>
-        <div className="block">
-          <label>用户名</label>
-          <input
-            name="username"
-            type="text"
-            value={this.state.username}
-            placeholder="请输入用户名"
-            onChange={this.handleChange}/>
+        <div className="container">
+          <div className="block title">
+            用户登陆
+          </div>
+          <div className="block">
+            <label>用户名</label>
+            <input
+              name="username"
+              type="text"
+              value={this.state.username}
+              placeholder="请输入用户名"
+              onChange={this.handleChange}/>
+            <p>{this.state.username_prompt}</p>
+          </div>
+          <div className="block">
+            <label>密码</label>
+            <input
+              name="password"
+              type="password"
+              value={this.state.password}
+              placeholder="请输入密码"
+              onChange={this.handleChange}/>
+            <p>{this.state.password_prompt}</p>
+          </div>
+          <div className="block">
+            <button className="blue" type="submit" onClick={this.handleClick}>登陆</button>
+          </div>
         </div>
-        <div className="block">
-          <label>密码</label>
-          <input
-            name="password"
-            type="password"
-            value={this.state.password}
-            placeholder="请输入密码"
-            onChange={this.handleChange}/>
-        </div>
-        <div className="block">
-          <button className="blue" type="submit" onClick={this.handleClick}>登陆</button>
-        </div>
-      </div>
       </div>
     );
   }
